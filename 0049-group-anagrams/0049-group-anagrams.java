f736d9c99c05d61d92e9f02212bf16d458c573d0
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-08-24 23:31:17 +0530

    Time: 6 ms (99.34%), Space: 49.8 MB (44.55%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -6,6 +6,7 @@ class Solution {
             char ch[]=strs[i].toCharArray();
             Arrays.sort(ch);
             String key=new String(ch);
+   //agar key ni h to key ke liye ek list add kro          
             if(!map.containsKey(key)){
                 map.put(key,new ArrayList<>());
             }
@@ -13,6 +14,6 @@ class Solution {
             map.get(key).add(strs[i]);
         }
         return new ArrayList<>(map.values());
-
+//bas values hamare kam ki hen, key to bas grping ke liye use hui hen , use it and throw it
     }
 }
