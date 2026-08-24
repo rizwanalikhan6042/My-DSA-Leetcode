5bb567b82c4edeef8ff7b7ea8e886ec9db1b6b6b
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-08-24 19:58:05 +0530

    Time: 0 ms (100%), Space: 48.4 MB (21.41%) - LeetHub

---
+++ 0344-reverse-string/0344-reverse-string.java
@@ -0,0 +1,14 @@
+class Solution {
+    public void reverseString(char[] s) {
+        int n=s.length;
+        if(n==1){
+            return;
+        }
+        for(int i=0;i<n/2;i++){
+            char temp=s[i];
+            s[i]=s[n-1-i];
+            s[n-1-i]=temp;
+        }
+        return;
+    }
+}
