fe97a317054d757cd4566709d5c82476cf60dfb1
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-08-24 08:41:33 +0530

    Time: 8 ms (34.74%), Space: 44.7 MB (30.91%) - LeetHub

--- 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.java
@@ -7,9 +7,12 @@ class Solution {
        int j=n-1;
        int ind=0;
        for(int i=n-1;i>=0;i--){
+//phla space jiske bad word h uski position tracking
         if(s.charAt(i)==' '&&s.charAt(i+1)!=' '){
             ind=i+1;
         }
+   //yani word ke last ch pr hen ham yahan
+   //yahan hamne substrng me ind se start kiya ku ki i+1 me to spaces bhi ho skte hen so we already saved the position of start as ind     
         if(s.charAt(i)==' '&&i>0&&s.charAt(i-1)!=' '){
             String str=s.substring(ind,j+1);
             sb.append(str+" ");
