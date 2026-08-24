e820d18fde351eed989850bd7cae6e08830dde00
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-08-24 20:09:23 +0530

    Time: 2 ms (99.65%), Space: 44.5 MB (66.69%) - LeetHub

---
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -0,0 +1,20 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+        int n=s.length();
+        int m=t.length();
+     int freq[]=new int[26];
+     for(int i=0;i<n;i++){
+        freq[s.charAt(i)-'a']++;
+     }
+     for(int i=0;i<m;i++){
+        freq[t.charAt(i)-'a']--;
+     }
+     for(int i=0;i<25;i++){
+        if(freq[i]!=0){
+            return false;
+        }
+     }
+     return true;
+
+    }
+}
