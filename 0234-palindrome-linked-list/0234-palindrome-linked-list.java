3d432db005543f62f337c8e7009a3899b9d5aab3
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-08-26 21:15:53 +0530

    Time: 3 ms (99.82%), Space: 94.5 MB (55.11%) - LeetHub

--- 0234-palindrome-linked-list/0234-palindrome-linked-list.java
+++ 0234-palindrome-linked-list/0234-palindrome-linked-list.java
@@ -10,21 +10,54 @@
  */
 class Solution {
     public boolean isPalindrome(ListNode head) {
-        ListNode temp=head;
-        List<Integer>arr=new ArrayList<>();
-        while(temp!=null){
-           arr.add(temp.val);
-           temp=temp.next;
+        //O(1) space cmplx
+        ListNode slow=head;
+        ListNode fast=head;
+        while(fast.next!=null&&fast.next.next!=null){
+            slow=slow.next;
+            fast=fast.next.next;
         }
-        int i=0;
-        int j=arr.size()-1;
-        while(i<j){
-          if(arr.get(i)!=arr.get(j)){
-            return false;
-          }
-          i++;
-          j--;
+        //slow is on the middle now 
+        ListNode prev=null;
+        ListNode curr=slow.next;
+        ListNode next=slow;
+        while(curr!=null){
+            next=curr.next;
+            curr.next=prev;
+            prev=curr;
+            curr=next;
+        }
+        //now prev is on last node
+        ListNode firstHead=head;
+        ListNode secHead=prev;
+
+        while(secHead!=null){
+            if(firstHead.val!=secHead.val){
+                return false;
+            }else{
+                firstHead=firstHead.next;
+                secHead=secHead.next;
+            }
         }
         return true;
+
+
+        //O(N) space cmplx
+        // ListNode temp=head;
+        // List<Integer>arr=new ArrayList<>();
+        // while(temp!=null){
+        //    arr.add(temp.val);
+        //    temp=temp.next;
+        // }
+        // int i=0;
+        // int j=arr.size()-1;
+        // while(i<j){
+        //   if(arr.get(i)!=arr.get(j)){
+        //     return false;
+        //   }
+        //   i++;
+        //   j--;
+        // }
+        // return true;
     }
 }
