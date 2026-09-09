0c70a484dc2d6dfd04d6f27d9c9b942ceef24c83
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-09 22:44:39 +0530

    Time: 35 ms (30.73%), Space: 47.3 MB (57.8%) - LeetHub

--- 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
+++ 1209-remove-all-adjacent-duplicates-in-string-ii/1209-remove-all-adjacent-duplicates-in-string-ii.java
@@ -10,26 +10,30 @@ class Solution {
     public String removeDuplicates(String s, int k) {
         int n=s.length();
         Stack<Pair>st=new Stack<>();
-        int c=0;
+   //actually we have to keep record of every char count thats why pair is needed     
         for(int i=0;i<n;i++){
             char curr=s.charAt(i);
           if(st.empty()||st.peek().ch!=curr){
             st.push(new Pair(curr,1));
-          }else {
-            st.peek().count++;
-            if(st.peek().count==k){
-                st.pop();
+          }else{
+            if(st.peek().ch==curr){
+                st.peek().count++;
+                if(st.peek().count==k){
+                    st.pop();
+                }
             }
-
           }
         }
-        StringBuilder ans= new StringBuilder();
-        while(!st.empty()){
-            Pair currentPair=st.pop();
-            for(int i=0;i<currentPair.count;i++){
-             ans.append(currentPair.ch);
-            }
+    StringBuilder ans=new StringBuilder();
+    while(!st.empty()){
+        Pair currentPair=st.pop();
+        for(int i=0;i<currentPair.count;i++){
+         ans.append(currentPair.ch);
         }
-                 return ans.reverse().toString();
+    }
+    return ans.reverse().toString();
+     
+
+
     }
 }
