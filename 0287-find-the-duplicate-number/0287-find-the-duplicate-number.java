a2b535b5cf7ebeef7864def8feef34116dd37986
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-11 15:25:08 +0530

    Time: 2 ms (98.66%), Space: 81.7 MB (85.6%) - LeetHub

---
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int findDuplicate(int[] nums) {
+    int freq[]=new int[nums.length];
+    for(int i=0;i<nums.length;i++){
+      freq[nums[i]]++;
+    }
+   for(int i=0;i<freq.length;i++){
+    if(freq[i]>=2){
+      return i;
+    }
+   }
+   return -1;
+    }
+}
