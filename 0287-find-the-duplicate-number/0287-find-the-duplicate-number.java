9168c7cb54e49ebc30cdae8664132787b74bc8d1
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-11 16:34:37 +0530

    Time: 4 ms (92.41%), Space: 83 MB (37.14%) - LeetHub

--- 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
+++ 0287-find-the-duplicate-number/0287-find-the-duplicate-number.java
@@ -2,6 +2,8 @@ class Solution {
     public int findDuplicate(int[] nums) {
     int tor=nums[0];
     int hare=nums[0];
+ //samjh lo cycle bn rhi h link list wali ab 
+ //isme tor hare algo lagega , phle cycle findount krni h fir dplicate   
     do{
         tor=nums[tor];
         hare=nums[nums[hare]];
