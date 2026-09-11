0b8f83de77d4cdac2bc5a9fb2e64e8a3dde63846
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-11 20:11:26 +0530

    Time: 6 ms (90.62%), Space: 57.8 MB (63.01%) - LeetHub

--- 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
+++ 0442-find-all-duplicates-in-an-array/0442-find-all-duplicates-in-an-array.java
@@ -4,11 +4,18 @@ class Solution {
         List<Integer>ans=new ArrayList<>();
 
         for(int i=0;i<n;i++){
+            //  Current element ki absolute (positive) value nikaali
+            // Kyunki pichle steps me koi element negative ho chuka ho sakta hai
             int val=Math.abs(nums[i]);
+//  Is value ke liye sahi index dhoonda (0-indexed array ke hisab se         
             int ind=(val)-1;
+//Agar is index par value abhi bhi positive hai,
+//iska matlab hum is index (yaani is ghar) par pehli baar aaye hain.        // Isko negative mark kar do, taaki yaad rahe hum yahan aa chuke hain (Taala laga diya)    
             if(nums[ind]>0){
                 nums[ind]=-1*nums[ind];
             }else{
+// DUPLICATE FOUND: Agar value pehle se hi negative mili,
+// iska matlab koi doosra element hume fir se isi index par bhej raha hai! // To hum samajh gaye ki 'val' hi duplicate hai, isko answer me add kar do               
                 ans.add(val);
             }
         }
