0a03906661a91ea8d2740741f5e7cd48876ce672
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-12 19:41:59 +0530

    Time: 11 ms (14.13%), Space: 49 MB (60.13%) - LeetHub

--- 0056-merge-intervals/0056-merge-intervals.java
+++ 0056-merge-intervals/0056-merge-intervals.java
@@ -1,18 +1,32 @@
 class Solution {
     public int[][] merge(int[][] intervals) {
-       int n=intervals.length;
-       Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
-       List<int[]>ans=new ArrayList<>();
-       for(int i=0;i<n;i++){
-        int s=ans.size();
-        if(ans.isEmpty()||(ans.get(s-1)[1]<intervals[i][0])){
-            ans.add(intervals[i]);
-        }else{
-            if(ans.get(s-1)[1]>=intervals[i][0]){
-                ans.get(s-1)[1]=Math.max(ans.get(s-1)[1],intervals[i][1]);
+        // int n = intervals.length;
+        // Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+        // List<int[]> ans = new ArrayList<>();
+        // for (int i = 0; i < n; i++) {
+        //     int s = ans.size();
+        //     if (ans.isEmpty() || (ans.get(s - 1)[1] < intervals[i][0])) {
+        //         ans.add(intervals[i]);
+        //     } else {
+        //         if (ans.get(s - 1)[1] >= intervals[i][0]) {
+        //             ans.get(s - 1)[1] = Math.max(ans.get(s - 1)[1], intervals[i][1]);
+        //         }
+        //     }
+        // }
+        // return ans.toArray(new int[ans.size()][]);
+        int n=intervals.length;
+        Arrays.sort(intervals,(a,b)->Integer.compare(a[0],b[0]));
+        List<int[]>ans=new ArrayList<>();
+        for(int i=0;i<n;i++){
+            int s=ans.size();
+            if(ans.isEmpty()||ans.get(s-1)[1]<intervals[i][0]){
+                ans.add(intervals[i]);
+            }else{
+                if(ans.get(s-1)[1]>=intervals[i][0]){
+                    ans.get(s-1)[1]=Math.max(ans.get(s-1)[1],intervals[i][1]);
+                }
             }
         }
-       }
-       return ans.toArray(new int[ans.size()][]);
+        return ans.toArray(new int[ans.size()][]);
     }
 }
