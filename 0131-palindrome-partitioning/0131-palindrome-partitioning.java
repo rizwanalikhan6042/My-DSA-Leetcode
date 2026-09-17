c27dc6a67c90efef101e51e582701edd1c4e07ec
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-18 00:28:08 +0530

    Time: 8 ms (73.37%), Space: 65.3 MB (40.62%) - LeetHub

--- 0131-palindrome-partitioning/0131-palindrome-partitioning.java
+++ 0131-palindrome-partitioning/0131-palindrome-partitioning.java
@@ -1,32 +1,39 @@
 class Solution {
     public List<List<String>> partition(String s) {
+        int n=s.length();
         List<List<String>>ans=new ArrayList<>();
         List<String>path=new ArrayList<>();
-        backtrack(s,0,path,ans);
+        backtrack(s,0,ans,path);
         return ans;
-    }
-    private void backtrack(String s,int start,List<String>path,List<List<String>>ans){
-        if(start>=s.length()){
-            ans.add(new ArrayList<>(path));
-            return;
         }
-        for(int i=start;i<s.length();i++){
-            if(isPal(s,start,i)){
-            String sub=s.substring(start,i+1);
-            path.add(sub);
-            backtrack(s,i+1,path,ans);
-            path.remove(path.size()-1);
+    private void backtrack(String s,int str,List<List<String>>ans,List<String>path){
+        if(str==s.length()){
+         ans.add(new ArrayList<>(path));
+         return;
+        }
+        for(int i=str;i<s.length();i++){
+            if(isPal(s,str,i)){
+                String sub=s.substring(str,i+1);
+                path.add(sub);
+                backtrack(s,i+1,ans,path);
+                path.remove(path.size()-1);
             }
         }}
-        private boolean isPal(String s,int low,int high){
-            while(low<high){
-           if(s.charAt(low)!=s.charAt(high)){
+     private boolean isPal(String s,int low,int high){
+        while(low<high){
+            if(s.charAt(low)!=s.charAt(high)){
                 return false;
             }
-            low++;
-            high--;
-            }
-            return true;
+            low++;high--;
         }
-}
+        return true;
+     }
+
     
+
+}
+ 
+
+
+
+ 
