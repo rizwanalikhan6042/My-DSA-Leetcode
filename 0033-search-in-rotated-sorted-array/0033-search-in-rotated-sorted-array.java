b6394840630459afea3180013aa2bbe11f478c93
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-20 21:29:04 +0530

    Time: 0 ms (100%), Space: 44.1 MB (11.91%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -1,28 +1,55 @@
 class Solution {
     public int search(int[] nums, int target) {
-        int n=nums.length;
-        int low = 0;
-        int high=n-1;
-       while(low<=high){
-        int mid=low+(high-low)/2;
+//         int n=nums.length;
+//         int low=0;
+//         int high=n-1;
+
+//         while(low<=high){
+//             int mid=low+(high-low)/2;
+//             if(nums[mid]==target){
+//                 return mid;
+//             }
+//             if(nums[low]<=nums[mid]){
+//                 if(target>=nums[low]&&target<nums[mid]){
+// //we already checked that nums[mid] is eq to trgt or not so no need to put equal sign here target<nums[mid] is enough                    
+//                     high=mid-1;
+//                 }else{
+//                     low=mid+1;
+//                 }
+//             }else{
+//                 if(target<=nums[high]&&target>nums[mid]){
+//                     low=mid+1;
+//                 }else{
+//                     high=mid-1;
+//                 }
+//             }
+//         }
+//         return -1;
+
+     int n=nums.length;
+     int i=0;
+     int j=n-1;
+     while(i<=j){
+        int mid=i+(j-i)/2;
         if(nums[mid]==target){
             return mid;
         }
-        if(nums[low]<=nums[mid]){
-         if(target>=nums[low]&&target<nums[mid]){
-            high=mid-1;
-
-         }else{
-            low=mid+1;
-         }
+        if(nums[i]<=nums[mid]){
+        if(target>=nums[i]&&target<nums[mid]){
+            j=mid-1;
         }else{
-          if(target<=nums[high]&&target>nums[mid]){
-           low=mid+1;
-          }else{
-            high=mid-1;
-          }
+            i=mid+1;
         }
-       }
-       return -1;
+        }else{
+            if(target>nums[mid]&&target<=nums[j]){
+                i=mid+1;
+
+            }else{
+                j=mid-1;
+            }
+        }
+     }
+
+return -1;
     }
 }
