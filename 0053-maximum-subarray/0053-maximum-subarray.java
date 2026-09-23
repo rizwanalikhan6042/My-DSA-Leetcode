39ce398ffef098b9edc0ba7edddd0c8b959a8b44
Rizwan Ali Khan <rizwanalikhan6042@gmail.com>
2026-09-23 15:22:50 +0530

    Time: 1 ms (99.93%), Space: 77 MB (91.91%) - LeetHub

--- 0053-maximum-subarray/0053-maximum-subarray.java
+++ 0053-maximum-subarray/0053-maximum-subarray.java
@@ -4,9 +4,9 @@ class Solution {
         int n=nums.length;
         int currMax=0;
         int maxSofar=Integer.MIN_VALUE;
-        if(n==1){
-            return nums[0];
-        }
+        // if(n==1){
+        //     return nums[0];
+        // }
         for(int i=0;i<n;i++){
             currMax=Math.max(nums[i],currMax+nums[i]);
             maxSofar=Math.max(currMax,maxSofar);
